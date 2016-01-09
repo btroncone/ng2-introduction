34dfb40ed566ac543ebc497542099e37ececb0fc
Brian Troncone <btroncone@gmail.com>
2016-01-08 19:40:46 -0500

    added wallaby config

--- spec-bundle.js
+++ spec-bundle.js
@@ -1,12 +1,4 @@
-/*
- * When testing with webpack and ES6, we have to do some extra
- * things get testing to work right. Because we are gonna write test
- * in ES6 to, we have to compile those as well. That's handled in
- * wallaby.js with the karma-webpack plugin. This is the entry
- * file for webpack test. Just like webpack will create a bundle.js
- * file for our client, when we run test, it well compile and bundle them
- * all here.
-*/
+/* Config used from AngularClass */
 Error.stackTraceLimit = Infinity;
 require('phantomjs-polyfill');
 require('es6-promise');
